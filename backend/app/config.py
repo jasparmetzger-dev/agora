782af4d3ae755bac5bffc8b54222e22ebbf83497
jasparmetzger-dev <jaspar.metzger@web.de>
2026-02-25 15:15:02 +0100

    idk what i last did

--- backend/app/config.py
+++ backend/app/config.py
@@ -1,10 +1,15 @@
 from pydantic_settings import BaseSettings
 
 class Settings(BaseSettings):
+    #db
     database_url: str
+
+    #jwt
     secret_key: str
 
+
+
     class Config:
-        env_file = ".env"
+        env_file = "backend/.env"
 
 settings = Settings()
