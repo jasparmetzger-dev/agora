dc82ad93d087b467a58518a38313e45821053a60
jasparmetzger-dev <jaspar.metzger@web.de>
2026-03-22 16:59:16 +0100

    making video testing possible

---
+++ scripts/curl_tests/test_upload.sh
@@ -0,0 +1,43 @@
+#!/usr/bin/env bash
+
+BASE_URL="http://localhost:8080"
+BASE_UPLOAD_URL="~/linux_repos/agora/testing_data"
+USERNAME="testuser_$RANDOM"
+EMAIL="$USERNAME@test.com"
+PASSWORD="password123"
+
+echo "=== Register $USERNAME ==="
+curl -s -X POST "$BASE_URL/register" \
+  -H "Content-Type: application/json" \
+  -d "{\"username\": \"$USERNAME\", \"email\": \"$EMAIL\", \"password\": \"$PASSWORD\"}" > /dev/null
+
+echo "=== Login ==="
+TOKEN=$(curl -s -X POST "$BASE_URL/login" \
+  -H "Content-Type: application/json" \
+  -d "{\"username\": \"$USERNAME\", \"password\": \"$PASSWORD\"}" | jq -r '.token')
+echo "Token: $TOKEN"
+
+AUTH="Authorization: Bearer $TOKEN"
+
+echo "=== Upload Videos"
+curl -s -X POST "$BASE_URL/posts" \
+    -H "$AUTH" \
+    -F "title=one" \
+    -F "description=desription one" \
+    -F "file=@$BASE_UPLOAD_URL/test_video_1.mp4"
+echo "=== 1 / 3 done ==="
+curl -s -X POST "$BASE_URL/posts" \
+    -H "$AUTH" \
+    -F "title=two" \
+    -F "description=description two" \
+    -F "file=@$BASE_UPLOAD_URL/test_video_2.mp4"
+echo "=== 2 / 3 done ==="
+curl -s -X POST "$BASE_URL/posts" \
+    -H "$AUTH" \
+    -F "title=three" \
+    -F "description=desription three" \
+    -F "file=@$BASE_UPLOAD_URL/test_video_3.mp4"
+echo "=== Uploading successful ==="
+
+curl  -X GET "$BASE_URL/posts" \
+    -H "$AUTH" -H "Content-Type: application/json" | jq
